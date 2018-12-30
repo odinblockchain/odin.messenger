4496d96c674ee55a6eef91694124daa689a56394
Manbearpixel <manbearpixel@gmail.com>
2018-12-30 03:39:57 -0600

    Added route for Contact Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 const routes: Routes = [
-  // { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: "", redirectTo: "/splashscreen", pathMatch: "full" },
+  // { path: "", redirectTo: "/contact-add", pathMatch: "full" },
   { path: "home", loadChildren: "~/app/home/home.module#HomeModule" },
   { path: "browse", loadChildren: "~/app/browse/browse.module#BrowseModule" },
   { path: "search", loadChildren: "~/app/search/search.module#SearchModule" },
   { path: "splashscreen", loadChildren: "~/app/splashscreen/splashscreen.module#SplashscreenModule" },
   { path: "settings", loadChildren: "~/app/settings/settings.module#SettingsModule" },
   { path: "create", loadChildren: "~/app/create-account/create-account.module#CreateAccountModule" },
-  { path: "messages", loadChildren: "~/app/messages/messages.module#MessagesModule" }
+  { path: "messages", loadChildren: "~/app/messages/messages.module#MessagesModule" },
+  { path: "contact-add", loadChildren: "~/app/contact-add/contact-add.module#ContactAddModule" }
 ];
 
 @NgModule({
