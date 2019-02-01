19562263f225417c7fbbd39df496487a21a66b35
Manbearpixel <manbearpixel@gmail.com>
2019-01-31 21:21:22 -0600

    Settings view UI adustments

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -4,6 +4,11 @@ import * as app from "tns-core-modules/application";
 import { UserModel } from '~/app/shared/user.model';
 import { PreferencesService } from '~/app/shared/preferences.service';
 import { alert, confirm } from "ui/dialogs";
+import { isAndroid, isIOS, device } from "platform";
+import { Page } from "ui/page";
+
+declare var android: any;
+declare var java: any;
 
 @Component({
   selector: "Settings",
@@ -14,11 +19,14 @@ import { alert, confirm } from "ui/dialogs";
 export class SettingsComponent implements OnInit {
   public preferences: any;
   public user: UserModel;
+  public darkMode: boolean;
   
   constructor(
+    private _page: Page,
     private _user: UserModel,
     private _pref: PreferencesService
   ) {
+    this.darkMode     = true;
     this.user         = this._user;
     this.preferences  = this._pref.preferences;
   }
@@ -60,6 +68,38 @@ export class SettingsComponent implements OnInit {
       } else {
         alert('Something unexpected occurred while publishing a new batch of one-time use Public Tokens to ODIN-Server. Please check your configurations and try again.');
       }
+    }
+  }
+
+  toggleUITheme() {
+    if (isAndroid && device.sdkVersion >= "21") {
+      let windowView = app.android.startActivity.getWindow();
+      let decorView = windowView.getDecorView();
+      console.log('>= 21', device.sdkVersion);
+
+      if (!this.darkMode) {
+        let colorHex = '#1b1b1b';
+
+        let colorInt = android.graphics.Color.parseColor(colorHex);
+        this._page.backgroundColor = colorHex;
+
+        console.log('colorInt', colorInt);
+        windowView.setStatusBarColor(colorInt);
+        decorView.setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_VISIBLE);
+      } else {
+        let colorHex = '#AAAAAA';
+
+        let colorInt = android.graphics.Color.parseColor(colorHex);
+        this._page.backgroundColor = colorHex;
+        
+        console.log('colorInt', colorInt);
+        windowView.setStatusBarColor(colorInt);
+        decorView.setSystemUiVisibility(android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+      }
+
+      this.darkMode= !this.darkMode;
+    } else {
+      console.log('not supported');
     }
   }
 }
