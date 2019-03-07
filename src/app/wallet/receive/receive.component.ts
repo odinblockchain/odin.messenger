b66f9eb380d4ffce846c7972bd2c15f465693f42
Manbearpixel <manbearpixel@gmail.com>
2019-03-07 08:41:42 +0000

    Wallet receive now displays actual external addresses and respective balances

--- src/app/wallet/receive/receive.component.ts
+++ src/app/wallet/receive/receive.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { WalletModel } from '~/app/shared/wallet.model';
 
 @Component({
 	moduleId: module.id,
@@ -7,13 +8,51 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./receive.component.css']
 })
 
-export class ReceiveComponent implements OnInit {
+export class ReceiveComponent implements OnInit, OnChanges {
   @Input() selectedWalletId: number;
+  @Input() currentWallet: any;
   @Output() walletSelected: EventEmitter<any> = new EventEmitter();
 
-	constructor() { }
+  public usedExternalAddresses: Array<any>;
+  public unusedAddress: any;
+
+	constructor(
+    private _wallet: WalletModel
+  ) {
+    this.usedExternalAddresses = [];
+    this.unusedAddress = {};
+  }
 
 	ngOnInit() { }
+
+  ngOnChanges(simpleChanges: any) {
+    console.log('[Wallet >> Receive] onChanges');
+    // console.dir(simpleChanges);
+
+    try {
+      if (this.currentWallet.external.length) {
+        this.usedExternalAddresses = this.currentWallet.external.filter(address => {
+          if (address.balance.confirmed > 0 || address.balance.unconfirmed > 0) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+  
+        this.unusedAddress = this.currentWallet.external.find(address => {
+          if (address.balance.confirmed === 0 && address.balance.unconfirmed === 0) {
+            return true;
+          } else {
+            return false;
+          }
+        });
+      } else {
+        this.usedExternalAddresses = [];
+      }
+    } catch(err) {
+      console.log('[Wallet Receive] Current Wallet not loaded yet');
+    }
+  }
 
   public proxyWalletSelection(selectedWalletId: number) {
     this.walletSelected.next(selectedWalletId);
