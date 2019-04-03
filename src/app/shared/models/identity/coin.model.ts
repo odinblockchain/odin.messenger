63e25274bf6573020441732caef851a6236b859f
Manbearpixel <manbearpixel@gmail.com>
2019-04-03 02:26:33 +0100

    Fleshed out Coin model

--- src/app/shared/models/identity/coin.model.ts
+++ src/app/shared/models/identity/coin.model.ts
@@ -2,10 +2,14 @@ import { Deserializable } from '../deserializable.model';
 
 export class Coin implements Deserializable {
   name: string;
+  is_default: boolean;
+  bip44: number;
   label: string;
   symbol: string;
   icon_path: string;
   explorer_host: string;
+  explorer_api_host: string;
+  explorer_api_stats: string;
   electrumx_host: string;
   electrumx_port: number;
 
@@ -14,6 +18,14 @@ export class Coin implements Deserializable {
   }
 
   deserialize(input: any) {
+    if (!input) return this;
+
+    if (`${input.is_default}` === '0' || input.is_default === 'false') {
+      input.is_default = false
+    } else if (`${input.is_default}` === "1" || input.is_default === 'true') {
+      input.is_default = true;
+    }
+
     Object.assign(this, input);
     return this;
   }
