ecd0c46260c40a7e138c969e8c87b8bdadaa0fb8
Manbearpixel <manbearpixel@gmail.com>
2019-04-04 11:40:58 +0100

    Coin is now serializable

--- src/app/shared/models/identity/coin.model.ts
+++ src/app/shared/models/identity/coin.model.ts
@@ -1,6 +1,7 @@
 import { Deserializable } from '../deserializable.model';
 
 export class Coin implements Deserializable {
+  // db
   name: string;
   is_default: boolean;
   bip44: number;
@@ -13,11 +14,11 @@ export class Coin implements Deserializable {
   electrumx_host: string;
   electrumx_port: number;
 
-  constructor(props: any) {
+  constructor(props?: any) {
     this.deserialize(props);
   }
 
-  deserialize(input: any) {
+  deserialize(input?: any) {
     if (!input) return this;
 
     if (`${input.is_default}` === '0' || input.is_default === 'false') {
@@ -28,5 +29,21 @@ export class Coin implements Deserializable {
 
     Object.assign(this, input);
     return this;
+  }
+
+  public serialize() {
+    return {
+      name: this.name,
+      is_default: this.is_default,
+      bip44: this.bip44,
+      label: this.label,
+      symbol: this.symbol,
+      icon_path: this.icon_path,
+      explorer_host: this.electrumx_host,
+      explorer_api_host: this.explorer_api_host,
+      explorer_api_stats: this.explorer_api_stats,
+      electrumx_host: this.electrumx_host,
+      electrumx_port: this.electrumx_port
+    };
   }
 }
