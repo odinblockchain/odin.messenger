cf0c089587ff01f7f2943d851e2482e5898f45cb
Manbearpixel <manbearpixel@gmail.com>
2019-04-13 11:09:03 -0500

    Cleanup Wallet Send

--- src/app/wallet/send/send.component.ts
+++ src/app/wallet/send/send.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { confirm, alert } from "tns-core-modules/ui/dialogs";
+import { Component, Input, OnInit } from '@angular/core';
+import { alert, confirm } from "tns-core-modules/ui/dialogs";
+import { Wallet } from '~/app/shared/models/wallet';
 import { WalletModel } from '~/app/shared/wallet.model';
 
 interface TransactionDetails {
   amount: number,
-  recipient: string
+  address: string
 };
 
 @Component({
@@ -14,9 +15,10 @@ interface TransactionDetails {
 	styleUrls: ['./send.component.css']
 })
 export class SendComponent implements OnInit {
-  @Input() selectedWalletId: number;
-  @Input() currentWallet: any;
-  @Output() walletSelected: EventEmitter<any> = new EventEmitter();
+  // @Input() selectedWalletId: number;
+  @Input() currentWallet: Wallet;
+  @Input() sendTransactionFn: any;
+  // @Output() walletSelected: EventEmitter<any> = new EventEmitter();
 
   public transactionDetails: TransactionDetails;
   public TX_FEE = 0.0001;
@@ -26,19 +28,28 @@ export class SendComponent implements OnInit {
   ) {
     this.transactionDetails = {
       amount: null,
-      recipient: null
+      address: null
     };
 
     this.onReturnPress = this.onReturnPress.bind(this);
-    this.processTransaction = this.processTransaction.bind(this);
+    // this.processTransaction = this.processTransaction.bind(this);
   }
 
   ngOnInit() { }
+
+  private formatBalance(amount: number): number {
+    return Number((amount / 1e8).toFixed(8));
+  }
   
   public onReturnPress(event): any {
-    console.log('send', this.currentWallet.balance);
 
-    if (!this.transactionDetails.recipient || this.transactionDetails.recipient.length === 0) {
+    if (!this.sendTransactionFn) return;
+
+    // console.log('send', this.currentWallet.balance);
+    console.log('BALANCE', this.currentWallet.balance_conf);
+    console.log('MIN', (this.TX_FEE + Number(this.transactionDetails.amount)) * 1e8);
+
+    if (!this.transactionDetails.address || this.transactionDetails.address.length === 0) {
       return alert('You must enter a valid ODIN Address to send ODIN too.');
     }
 
@@ -46,53 +57,60 @@ export class SendComponent implements OnInit {
       return alert('You must enter a valid amount of ODIN to send.');
     }
 
-    let walletBalance = this.currentWallet.balance.confirmed;
-    let amountNeeded = (this.TX_FEE + Number(this.transactionDetails.amount)) * 1e8;
+    const sendAmount = (this.TX_FEE + Number(this.transactionDetails.amount)) * 1e8;
 
-    if (walletBalance < amountNeeded) {
-      return alert(`The amount you are trying to send exceeds your current balance of ${(walletBalance / 1e8).toFixed(8)} ODIN. All transactions incur a fee of ${this.TX_FEE} ODIN.\n\nPlease adjust the amount or deposit more ODIN into your mobile wallet.`);
+    
+
+    if (this.currentWallet.balance_conf < sendAmount) {
+      return alert('Transaction failed!\n\n'
+      +`Wallet Balance:     ${this.formatBalance(this.currentWallet.balance_conf)} Ø\n\n`
+      +`Transaction Fee:    ${this.TX_FEE} Ø\n\n`
+      +`Transaction Total:  ${this.formatBalance(sendAmount)}`);
     }
     
     confirm({
       title: "Confirm Transaction",
-      message: `Please confirm your transaction of ${(amountNeeded / 1e8).toFixed(8)} ODIN to ${this.transactionDetails.recipient}.\n\nA transaction fee of ${this.TX_FEE} ODIN has been applied.\n\n`,
-      okButtonText: "Confirm",
+      message: `Sending ${this.transactionDetails.amount} ODIN\n`
+                + `To ${this.transactionDetails.address}\n\n`
+                + 'Send this transaction?',
+      okButtonText: "Send",
       cancelButtonText: "Cancel Transaction",
     })
     .then(result => {
       console.log(result);
-      if (result) this.processTransaction(this.transactionDetails);
+      console.log('working');
+      if (result) this.sendTransactionFn(this.transactionDetails);
     });
   }
 
-  private processTransaction({ recipient, amount }) {
-    console.log(`Sending [${amount}] to [${recipient}]`);
-    let unspent = this.currentWallet.unspent.slice(0).sort((tx1, tx2) => {
-      if (tx1['height'] < tx2['height']) return -1;
-      else if (tx1['height'] > tx2['height']) return 1;
-      return 0;
-    });
+  // private processTransaction({ address, amount }) {
+  //   console.log(`Sending [${amount}] to [${address}]`);
+  //   let unspent = this.currentWallet.unspent.slice(0).sort((tx1, tx2) => {
+  //     if (tx1['height'] < tx2['height']) return -1;
+  //     else if (tx1['height'] > tx2['height']) return 1;
+  //     return 0;
+  //   });
 
-    let coinControl = unspent.reduce((txArr, tx) => {
-      let sum = txArr.reduce((sum, _tx) => sum += _tx['value'], 0);
-      if (sum <= (amount * 1e8)) {
-        txArr.push(tx);
-      }
+  //   let coinControl = unspent.reduce((txArr, tx) => {
+  //     let sum = txArr.reduce((sum, _tx) => sum += _tx['value'], 0);
+  //     if (sum <= (amount * 1e8)) {
+  //       txArr.push(tx);
+  //     }
 
-      return txArr;
-    }, []);
+  //     return txArr;
+  //   }, []);
 
-    this._wallet.sendTransaction(this.currentWallet, recipient, amount, coinControl, this.TX_FEE)
-    .then((txid) => {
-      alert(`Transaction Sent!\n\nTXID:\n${txid}\n\nPlease allow a few minutes for this transaction to appear in your mobile wallet.`);
-    })
-    .catch((err) => {
-      console.log('err', err);
-      alert('error');
-    })
-  }
+  //   this._wallet.sendTransaction(this.currentWallet, address, amount, coinControl, this.TX_FEE)
+  //   .then((txid) => {
+  //     alert(`Transaction Sent!\n\nTXID:\n${txid}\n\nPlease allow a few minutes for this transaction to appear in your mobile wallet.`);
+  //   })
+  //   .catch((err) => {
+  //     console.log('err', err);
+  //     alert('error');
+  //   })
+  // }
 
-  public proxyWalletSelection(selectedWalletId: number) {
-    this.walletSelected.next(selectedWalletId);
-  }
+  // public proxyWalletSelection(selectedWalletId: number) {
+  //   this.walletSelected.next(selectedWalletId);
+  // }
 }
