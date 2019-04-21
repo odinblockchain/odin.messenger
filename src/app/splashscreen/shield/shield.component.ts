cfffb1106858625c4b46aff08c1834d40872318b
Manbearpixel <manbearpixel@gmail.com>
2019-04-20 23:53:12 -0500

    Shield slide component

---
+++ src/app/splashscreen/shield/shield.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ShieldConversationScreen",
+  moduleId: module.id,
+  templateUrl: "./shield.component.html",
+  styleUrls: ['./shield.component.css']
+})
+export class ShieldConversationScreenComponent {
+  constructor() { }
+}
