b4703b4a1a1d93c52fd201cb365e514111c39800
Manbearpixel <manbearpixel@gmail.com>
2019-05-12 01:54:25 -0500

    Extra cleanup

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,12 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
-
-// import { NativeScriptUIListViewModule } from 'nativescript-ui-listview/angular';
-// import { NativeScriptUICalendarModule } from 'nativescript-ui-calendar/angular';
-// import { NativeScriptUIChartModule } from 'nativescript-ui-chart/angular';
-// import { NativeScriptUIDataFormModule } from 'nativescript-ui-dataform/angular';
-// import { NativeScriptUIAutoCompleteTextViewModule } from 'nativescript-ui-autocomplete/angular';
-// import { NativeScriptUIGaugeModule } from 'nativescript-ui-gauge/angular';
 import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 
 import { SettingsRoutingModule } from './settings-routing.module';
