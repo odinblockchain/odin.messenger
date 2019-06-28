360feb9d8dd5573315a2364226efa4a413825839
Manbearpixel <manbearpixel@gmail.com>
2019-06-28 12:21:05 -0500

    Fix relative time for timestamps

--- src/app/utils/relativeTime.ts
+++ src/app/utils/relativeTime.ts
@@ -11,11 +11,11 @@ export class RelativeTime implements PipeTransform {
 
   transform(blocktime: number): string {
     try {
-      if (blocktime === 0) {
+      if (blocktime === 0 || isNaN(blocktime)) {
         return '—';
       }
 
-      let timeFormats = [
+      const timeFormats = [
         'year',
         'month',
         'week',
@@ -25,32 +25,25 @@ export class RelativeTime implements PipeTransform {
         'second'
       ];
 
-      let now   = moment();
-      let then  = moment(Number(blocktime) * 1000);
+      if (`${blocktime}`.length <= 10) blocktime = Number(blocktime) * 1000;
 
-      let format: any = timeFormats.find((time: any) => {
+      const now   = moment();
+      const then  = moment(blocktime);
+
+      const format: any = timeFormats.find((time: any) => {
         return (now.diff(then, time) > 0)
       });
       
-      let timeDiff = now.diff(then, format);
+      const timeDiff = now.diff(then, format);
       if (timeDiff === 1) return `${timeDiff} ${format} ago`;
       else return `${timeDiff} ${format}s ago`;
-
-      // return `>>${format}`;
-
-      // if (now.diff(then, 'years') > 0) return `${now.diff(then, 'years')} years ago`;
-      // else if (now.diff(then, 'months') > 0) return `${now.diff(then, 'months')} months ago`;
-      // else if (now.diff(then, 'weeks') > 0) return `${now.diff(then, 'weeks')} weeks ago`;
-      // else if (now.diff(then, 'days') > 0) return `${now.diff(then, 'days')} days ago`;
-      // else if (now.diff(then, 'hours') > 0) return `${now.diff(then, 'hours')} hours ago`;
-      // else if (now.diff(then, 'minutes') > 0) return `${now.diff(then, 'minutes')} minutes ago`;
-      // else if (now.diff(then, 'seconds') > 0) return `${now.diff(then, 'seconds')} seconds ago`;
-      
-      // if (now.diff())
-      // return `nice>${blocktime}`;
     } catch (err) {
-      console.log('Failed to convert blocktime');
-      return 'NaN';
+      console.log(`[App::Util::RelativeTime]
+        Error: Failed to convert blocktime
+        Blocktime: ${blocktime}
+      `);
+
+      return '—';
     }
   }
 }
